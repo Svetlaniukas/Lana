e22311600a65500c2fab7ed386c7b98c0d39acbf
Svetlaniukas <113213258+Svetlaniukas@users.noreply.github.com>
2022-11-11 10:03:36 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("main.html")
+
+
+@app.route('/staff')
+def staff():
+    return render_template("staff.html")
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return "User page:" + name + str(id)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
