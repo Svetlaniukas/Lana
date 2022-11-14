764233b9208f06cd74c47031d010e035cc0fc310
Svetlaniukas <svetlana.melichova@gmail.com>
2022-11-14 14:36:54 +0000

    add master.txt

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
+import datetime
+
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask (__name__)
 
-
-@app.route('/')
-@app.route('/home')
+@app.route ('/')
+@app.route ('/home')
 def home():
-    return render_template("home.html")
+    today = datetime.date.today( )
+    from_file = readfile("master.txt")
+    return render_template ("home.html")
 
 
-
-@app.route('/staff')
+@app.route ('/staff')
 def staff():
-    return render_template("staff.html")
+    today = datetime.date.today( )
+    return render_template ("staff.html")
 
-
-@app.route('/user/<string:name>/<int:id>')
+@app.route
 def user(name, id):
-    return "User page:" + name + str(id)
+    return "User page:" + name + str (id)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run (debug=True)
