2353028a3f15005abc2e725bc95ebc26fa1b0816
Svetlaniukas <svetlana.melichova@gmail.com>
2022-11-16 12:01:04 +0000

    add calendar

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import calendar
 from datetime import datetime
 
 from flask import Flask, render_template
@@ -10,23 +11,27 @@ app = Flask (__name__)
 @app.route ('/home')
 def home():
     today = datetime.now( )
-    page_content = render_template(
-        "home.html",
-        curent_year = today.year
+    c = calendar.TextCalendar(calendar.SUNDAY)
+    page_content = render_template\
+        ("home.html",
+        current_year = today.year,
+        calendar_year = c.prmonth(2022,1,0,0)
     )
 
-    return page_content 
+    return page_content
 
 @app.route ('/staff')
 def staff():
     today = datetime.now( )
-    calendar_itiem = "123"
-    return render_template (
-        "staff.html",
-        curent_year = today.year,
-        calendar_itiem = calendar_itiem
+    c = calendar.TextCalendar( )
+    page_content =  render_template \
+        ("staff.html",
+        current_year = today.year,
+        calendar_year = c.formatyear(2002, w=2, l=1, c=6, m=3 )
 
     )
+
+    return page_content
 
 @app.route
 def user(name, id):
