2bf15a5969f73605c7b9e17d6d09b669e0a93657
Svetlaniukas <svetlana.melichova@gmail.com>
2022-11-18 16:34:10 +0000

    add. some change

--- app.py
+++ app.py
@@ -23,17 +23,18 @@ def home():
 @app.route ('/staff')
 @app. route ('/')
 def staff():
-    today = datetime.now( )
-    c = calendar.TextCalendar()
-    page_content =  render_template("staff.html",
+        today = datetime.now( )
+        c = calendar.TextCalendar()
+        staff = ("master.txt" 'wr')
+
+        page_content =  render_template("staff.html",
         current_year = today.time,
         calendar_year = c.formatyear(2002, w=2, l=1, c=6, m=3 ),
-        staff_page =  (10-4= monday, 9-3= tuesday, 10-5 = wendsday, 8-4 = friday)
+        staff = staff
 
         )
-    return page_content
 
-
+        return page_content
 
 if __name__ == "__main__":
     app.run (debug=True)
