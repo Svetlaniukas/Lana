18d625a9b1cce60ff13b171987652b2bcd66cdf7
Svetlaniukas <svetlana.melichova@gmail.com>
2022-11-21 18:09:31 +0000

    read time tamble from file and pass to template

--- app.py
+++ app.py
@@ -2,39 +2,43 @@ import calendar
 from datetime import datetime
 from flask import Flask, render_template
 
-
-
 app = Flask (__name__)
 
 @app.route ('/')
 @app.route ('/home')
 def home():
     today = datetime.now( )
-    c = calendar.TextCalendar(calendar.SUNDAY)
-    page_content =render_template("home.html",
-        current_year = today.year,
-        calendar_year = c.prmonth(2022,1,0,0),
+    c = calendar.TextCalendar()
+    page_content = render_template("home.html",
+     current_year = today.year,
+     calendar_year = c.formatyear(2002, w=2, l=1, c=6, m=3)
 
     )
-
     return page_content
 
 
 @app.route ('/staff')
-@app. route ('/')
 def staff():
-        today = datetime.now( )
-        c = calendar.TextCalendar()
-        staff1 = ("master.txt" 'wr')
+    
+    today = datetime.now( )
+    c = calendar.TextCalendar()
+    file = open ("master.txt", 'r')
+    time_table = {}
+    for line in file:
+        (key, val) = line.split (':')
+        time_table[key] = val
 
-        page_content =  render_template("staff.html",
-        current_year = today.time,
-        calendar_year = c.formatyear(2002, w=2, l=1, c=6, m=3 ),
-        staff =  staff1 in "__main__"
 
-        )
+    page_content =  render_template(
+        "staff.html",
+         current_year = today.year,
+         calendar_year =c.formatyear(2002, w=2, l=1, c=6, m=3),
+         staff_fale = time_table
+    )
 
-        return page_content
+
+    return page_content
+
 
 if __name__ == "__main__":
     app.run (debug=True)
---
+++ test.py
@@ -0,0 +1,7 @@
+file = open("master.txt",'r')
+time_table = {}
+for line in file :
+    print (line)
+    (key, val) = line.split ( ':' )
+    time_table[key] = val
+print (time_table)
