5208d7eabc989e04a52560d3334b7597f64dd744
Svetlaniukas <svetlana.melichova@gmail.com>
2022-11-23 12:32:23 +0000

    add file staff.weekend.txt  with weekend time extends it in base file staff.html

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import calendar
 from datetime import datetime
 from flask import Flask, render_template
-import pandas as pd
+
+
+
 app = Flask (__name__)
 
 @app.route ('/')
@@ -11,7 +13,7 @@ def home():
     c = calendar.TextCalendar ( )
     page_content = render_template ("home.html",
      current_year = today.year,
-     calendar_year =c .formatyear (2002, w=2, l=1, c=6, m=3)
+     calendar_year = c.formatyear (2002, w=2, l=1, c=6, m=3)
      )
     return page_content
 
@@ -19,22 +21,29 @@ def home():
 def staff():
     today = datetime.now ( )
     c = calendar.TextCalendar ( )
-    file = open ("master.txt", 'r+')
+    file = open ("master.txt", 'r')
     time_table = {}
     for line in file:
         (key, val) = line.split(':')
         time_table[key] = val
 
+    file = open ("staff.weekend.txt", 'r')
+    time_ = {}
+    for line in file:
+            (key, val) = line.split (':')
+            time_[key] = val
+
+
     page_content = render_template (
         "staff.html",
         current_year = today.year,
         calendar_year = c.formatyear (2002, w=2, l=1, c=6, m=3),
-        staff_table = pd.DataFrame
+        staff_file = time_table,
+        staff_time = time_
     )
 
     return page_content
 
-
-
 if __name__ == "__main__":
     app.run (debug=True)
+    
