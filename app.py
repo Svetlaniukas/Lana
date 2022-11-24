50f2f0a0d9ba50fbc6b0f55ef60e3b07bea75a08
Svetlaniukas <svetlana.melichova@gmail.com>
2022-11-24 14:13:06 +0000

    try to convert .txt files in dictionary function

--- app.py
+++ app.py
@@ -19,23 +19,18 @@ def home():
 
 @app.route ('/staff')
 def staff():
-    file = open ("staff_week_day.txt", 'r')
-    file = open ("staff.weekend.txt", 'r')
-    
-    for line in file:
-        def staff_shift(file_week_day, file_weekend):
-            staff_dictionary = dict (zip (file_week_day, file_weekend))
-            for lines in staff_dictionary:
-                (key, val) = lines.split (':')
-                staff_dictionary[key] = val
+    staff_week_day = open ("staff_week_day.txt", 'r')
+    staff_weekend= open ("staff.weekend.txt", 'r')
+def staff_shift(staff_week_day=None, staff_weekend=None):
+     staff_dictionary = dict (zip (staff_week_day, staff_weekend))
+     for lines in staff_dictionary:
+         (key, val) = lines.split (':')
+         staff_dictionary[key] = val
+     page_content = render_template ("staff.html",
+                 current_time_and_date = staff_dictionary,
+     )
 
-                page_content = render_template (
-                    "staff.html",
-                    current_time_and_date = staff_dictionary
-
-                )
-
-    return page_content
+     return page_content
 
 if __name__ == "__main__":
     app.run (debug=True)
