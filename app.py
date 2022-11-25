6e46128825c6c0fbb349f90c4fd736c791a77856
Svetlaniukas <svetlana.melichova@gmail.com>
2022-11-25 12:49:49 +0000

    non working staff function

--- app.py
+++ app.py
@@ -1,37 +1,43 @@
 import calendar
 from datetime import datetime
+
 from _CAL.Holiday import today
 from flask import Flask, render_template
-from itertools import chain
+
 
 app = Flask (__name__)
 
 @app.route ('/')
 @app.route ('/home')
 def home():
-    date_time = datetime.now ( )
-    calendar_year = calendar.TextCalendar ( )
-    page_content = render_template ("home.html",
-     current_time = today.year,
-     calendar_year = calendar_year.formatyear (2002, w=2, l=1, c=6, m=3)
-     )
-    return page_content
+        date_time = datetime.now ( )
+        calendar_year = calendar.TextCalendar ( )
+        page_content = render_template ("home.html",
+        current_time = today.year,
+        calendar_year = calendar_year.formatyear (2002, w=2, l=1, c=6, m=3)
 
+
+         return page_content
+
+
+for_week_days_shift = open ("staff_week_day.txt", 'r')
+for_staff_weekend_shift = open ("staff.weekend.txt", 'r')
+staff_file = {}
 @app.route ('/staff')
-def staff(current_staff_shift):
-    staff_week_day = open("staff_week_day.txt", 'r')
-    staff_weekend = open ("staff.weekend.txt", 'r')
- 
-    def items(current_staff_shift ):
-        current_staff_shift = dict (chain (staff_week_day .items ( ), staff_weekend.items ( )))
+def staff(staff_weekend=None, staff_week_day=None):
+         today = datetime.now ( )
+         for line in staff_weekend:
+             (key, val) = line.split ('=')
+         for line in staff_week_day:
+             (key, val) = line.split (':')
+         page_content = render_template ("staff.html",
+                        file_for_week_day=for_week_days_shift,
+                        file_for_weekend=for_staff_weekend_shift
 
-    page_content = render_template ("staff.html",
-    current_time_and_date = current_staff_shift,
-    )
-   
-    return page_content
+                                        )
 
+         return page_content
 
-if __name__ == "__main__":
-    app.run (debug=True)
+     if __name__ == "__main__":
+         app.run (debug=True)
 
