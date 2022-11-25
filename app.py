61b494d81a925a6eb0bcfe0643e91894a20e1f08
Svetlaniukas <svetlana.melichova@gmail.com>
2022-11-25 17:52:41 +0000

    add read_ftaff_from_file function

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import calendar
 from datetime import datetime
+
+import items as items
 from _CAL.Holiday import today
 from flask import Flask, render_template
 
@@ -12,39 +14,28 @@ def home():
         date_time = datetime.now ( )
         calendar_year = calendar.TextCalendar ( )
         page_content = render_template ("home.html",
-                       current_time = today.year,
+                       current_time = date_time ,
                        calendar_year = calendar_year.formatyear (2002, w=2, l=1, c=6, m=3)
 
                                         )
         return page_content
-
-
-def read_staff_from_file():
+def read_staff_from_file(file_name,file_delemiter):
     dict_staff = {}
-    for_week_days_shift = open ("staff_week_day.txt", 'r')
-    for_staff_weekend_shift = open ("staff_weekend.txt", 'r')
+    for_week_days_shift = open (file_name ,'r')
     for line in for_week_days_shift:
-        (key, val) = line.split (':')
+        (key, val) = line.split(file_delemiter)
         dict_staff[key] = val
-    for line in for_staff_weekend_shift:
-        (key, val) = line.split ('=')
-        dict_staff[key] = val
-
-        return
+    return dict_staff
 
 @app.route ('/staff')
-def staff(dict_staff):
-         for_staff_weekend_shift = read_staff_from_file()
-         for_week_days_shift = read_staff_from_file()
+def staff():
+         for_week_days_shift = read_staff_from_file ("staff_week_day.txt", ":")
+         for_staff_weekend_shift = read_staff_from_file ("staff_weekend.txt","=")
          page_content = render_template ("staff.html",
                         for_week_days_shift = for_week_days_shift,
                         for_staff_weekend_shift = for_staff_weekend_shift
                                          )
-
-
          return page_content
-
-
 if __name__ == "__main__":
          app.run (debug=True)
         
