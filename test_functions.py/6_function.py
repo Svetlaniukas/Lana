a4d438f6c6dc61c9b2acfecc24a820e41baa5e71
Svetlaniukas <svetlana.melichova@gmail.com>
2022-11-30 13:44:11 +0000

    add test. Function file.

--- test_functions.py/6_function.py
+++ test_functions.py/6_function.py
@@ -1,1 +1,86 @@
 
+# movie = 'The good,the bad, and the ungly'
+# rating = 100
+
+# result = f'Movie: "{movie}", rating: {rating}'
+# print(result)
+
+# print()
+
+# movie = 'Alien'
+#  rating = 200
+
+# result = f'Movie: "{movie}", rating: {rating}'
+# print(result)
+
+
+# def greet(name):
+    # print('Hello, name)
+    # result = f'Hello {name}'
+    # print(result)
+
+    # greet('world')
+    # greet('oleg')
+
+
+# def greet(name):
+      # print('Hello, name)
+      # result = f'{massage}, {name}'
+      # print (result)
+
+       # greet ('world, 'Goodbye')
+
+ # def greet(name):
+    # print('Hello, name)
+    # result = f'{massage}, {name}'
+    # print (result)
+
+     # greet(massage='world, name='Goodbye')
+
+ # def greet(name):
+      # print('Hello, name)
+      # result = f'{massage}, {name}'
+      # print (result)
+
+      # greet (massage='Hello')
+
+
+# def greet(name):
+    # print('Hello, name)
+    # result = f'{massage}, {name}'
+    # print (result)
+
+    # greet ( name= 'asdf', 'Hello')
+
+
+# greeting = 'Hello'
+# token = 'world'
+
+# def greet(name):
+    # print('Hello, name)
+    # result = f'{massage}, {name}'
+    # print (result)
+
+    # greet (greeting , to)
+
+    # massage = greeting = 'hello'
+
+ # def greet(name):
+        # print('Hello, name)
+     # result = f'{massage}, {name}'
+     # return result
+
+# g = greet(greeting, to)
+# print(g)
+
+# print(greet('hello))
+# g = greet('                  hello').title().strip()
+# print(g)
+#
+
+
+
+
+
+
+
