e1a104780f9e6065d40ed7409e506386ef2f68b9
Svetlaniukas <svetlana.melichova@gmail.com>
2022-12-01 14:23:32 +0000

    add work with some function -/*

--- test_functions.py/6_function.py
+++ test_functions.py/6_function.py
@@ -8,9 +8,6 @@ print(result)
 
 
 def greet2():
-    """
-    :param movie:
-    """
     result2 = f'Movie: {movie}'
     print(result2)
 
@@ -18,99 +15,33 @@ greet2()
 
 
 def greet3(movie='good_day', rating=300, ):
-    """
-
-    :param movie:
-    :param rating:
-    """
-    result3 = f'City:{movie}, {rating}'
-    print(result3)
+    result1 = f'{movie}, {rating}'
+    print(result1)
 
 greet3()
-
-
-def greet4(massage='world', name='oleg'):
-    """
-
-    :param massage:
-    :param name:
-    """
-    result4 = f'Movie: {massage}, {name}'
-    print(result4)
-
-greet4()
+greet3('Alien3', 100)
+greet3('Terminator', 34)
 
 
 
+def sum(first='numeric', second=33):
+    result = first + second
+    print(result)
 
-def greet5(massage='Hello'):
-    """
-
-    :param massage:
-    """
-    result5 = f'massage{massage}'
-    print(result5)
-
-greet5()
-
-
-def greet6(massage='Hello', name='asdf'):
-    """
-
-    :param massage:
-    :param name:
-    """
-    result6 = f'{massage}, {name}'
-    print(result6)
-
-greet6()
+sum(5, 7)
+sum('summa:', str(10 - 2))
+print()
+sum('all is good', str(123))
+print()
+sum(int(10), int(-5))
+print()
+sum('result:', str(10 * 5))
+print()
+sum('result:', str(10 / 2))
+print()
 
 
-# greet ( name= 'asdf', 'Hello')
 
 
-# greeting = 'Hello'
-# token = 'world'
-
-def greet7(name='greeting', massage='to'):
-    """
-
-    :param name:
-    :param massage:
-    """
-    result7 = f' {name} {massage}, '
-    print(result7)
-
-greet7()
-
-
-# greet (greeting , to)
-
-# massage = greeting = 'hello'
-
-def greet8(name='greeting', massage='to'):
-    """
-
-    :param name:
-    :param massage:
-    :return:
-    """
-    g = f'str{massage}, {name}'
-    return (g)
-
-greet8()
-
-
-# g = greet(greeting, to)
-# print(g)
-
-def greet9(name='hello'):
-    """
-    :param name:
-    """
-    g = f'greet{name}'
-    print(g)
-
-greet9()
 
 
