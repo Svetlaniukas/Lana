9448ea2a7f3a7cb60e1d17e692a93ec2c2c68d6a
Svetlaniukas <svetlana.melichova@gmail.com>
2022-12-03 19:52:09 +0000

    ad file with pytest testing

---
+++ test_function/test_division.py
@@ -0,0 +1,8 @@
+from my_function import greet
+
+
+
+def test_film(movie='Alien', rating = 200):
+    assert greet() == 'Movie: 'Alien', rating: 200'
+
+
--- test_function/test_division_function.py
+++
@@ -1,6 +0,0 @@
-from utils import division
-
-def test_division_good():
-    assert division(10, 5) == 5
-
-
