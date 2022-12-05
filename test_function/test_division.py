2a392fc5b66cd456b08da8a5e46993d4cc9e2333
Svetlaniukas <svetlana.melichova@gmail.com>
2022-12-05 11:59:26 +0000

    ad file with pytest testing

--- test_function/test_division.py
+++ test_function/test_division.py
@@ -1,8 +1,38 @@
-from my_function import greet
+import numbers
+from os import name
+
+from my_function import (
+    greet, greet2, greet3,
+    sum_plius, sum_minus, num_multi, num_divide, numeric)
 
 
+def test_greet():
+    assert greet('Alien', 200) == 'Movie: "Alien", rating: 200'
 
-def test_film(movie='Alien', rating = 200):
-    assert greet() == 'Movie: 'Alien', rating: 200'
+
+def test_greet2():
+    assert greet2('Terminator') == 'Movie: Terminator'
 
 
+def test_movie():
+    assert greet3('Sunny', 300) == 'Sunny 300'
+
+
+def test_sum():
+    assert sum_plius(5, 7) == 12
+
+
+def test_sum_minus():
+    assert sum_minus(10, 2) == ('summa:', 8)
+
+
+def test_sum_multiplay():
+    assert num_multi(10, 5) == ('result:', 50)
+
+
+def test_num_divide():
+    assert num_divide(10, 2) == ('result:', 5)
+
+
+def test_numeric(sur_name):
+    assert numeric({numbers}, {name}, {sur_name}) == (20, 'Tania', 'Bal')
