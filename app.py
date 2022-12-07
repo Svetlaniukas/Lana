730d8874cf5ef504638275c26093667bf1da21ad
Svetlaniukas <svetlana.melichova@gmail.com>
2022-12-07 17:40:54 +0000

    add app.py

--- app.py
+++ app.py
@@ -1,28 +1,25 @@
 import calendar
 from datetime import datetime
-
-import items as items
-from _CAL.Holiday import today
 from flask import Flask, render_template
 
 
-app = Flask (__name__)
+app = Flask(__name__)
 
 @app.route ('/')
-@app.route ('/home')
+@app.route('/home')
 def home():
-    date_time = datetime.now ( )
-    calendar_year = calendar.TextCalendar ( )
-    page_content = render_template ("home.html",
-    current_time = date_time ,
-    calendar_year = calendar_year.formatyear(format())
-
+    date_time = datetime.now()
+    calendar_year = calendar.TextCalendar()
+    page_content = render_template("home.html",
+                                   current_time=date_time,
+                                   calendar_year=calendar_year.formatyear(2023, w=2, l=1, c=6, m=3)
+                                   )
     return page_content
 
 
-def read_staff_from_file(file_name,file_delemiter):
+def read_staff_from_file(file_name, file_delemiter):
     dict_staff = {}
-    for_week_days_shift = open (file_name ,'r')
+    for_week_days_shift = open (file_name,'r')
     for line in for_week_days_shift:
         (key, val) = line.split(file_delemiter)
         dict_staff[key] = val
@@ -30,13 +27,14 @@ def read_staff_from_file(file_name,file_delemiter):
 
 @app.route ('/staff')
 def staff():
-         for_week_days_shift = read_staff_from_file ("staff_week_day.txt", ":")
-         for_staff_weekend_shift = read_staff_from_file ("staff_weekend.txt","=")
-         page_content = render_template ("staff.html",
-                        for_week_days_shift = for_week_days_shift,
-                        for_staff_weekend_shift = for_staff_weekend_shift
-                                         )
-         return page_content
+    for_week_days_shift = read_staff_from_file("staff_week_day.html", ":")
+    for_staff_weekend_shift = read_staff_from_file("staff_weekend.html", "=")
+    page_content = render_template("staff.html",
+                                   for_week_days_shift = for_week_days_shift,
+                                   for_staff_weekend_shift = for_staff_weekend_shift
+                                   )
+    return page_content
+
+
 if __name__ == "__main__":
-         app.run (debug=True)
-        
+    app.run (debug=True)
