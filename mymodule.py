ea37ffddfbd2c23dbb0efa6bc2775c3ec8d2180e
Svetlaniukas <svetlana.melichova@gmail.com>
2022-12-12 17:23:20 +0000

    model addline rstrip function

--- mymodule.py
+++ mymodule.py
@@ -7,6 +7,7 @@ def open_staff_file(file_name, file_delimiter):
     for line in for_staff_file:
         if file_name in file_delimiter:
             line.rstrip()
+        else:
             (key, val) = line.split(file_delimiter)
             dict_staff[key] = val
             return (key, val), dict_staff
