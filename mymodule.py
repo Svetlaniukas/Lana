44dc01790219ed395e28c70461a64828d8005042
Svetlaniukas <svetlana.melichova@gmail.com>
2022-12-13 14:10:37 +0000

    fixed val/key '\n'

--- mymodule.py
+++ mymodule.py
@@ -7,7 +7,12 @@ def open_staff_file(file_name, file_delimiter):
     for line in for_staff_file:
         if file_delimiter in line:
             (key, val) = line.split(file_delimiter)
+            key.strip()
+            val.strip('\n')
+            key.strip()
+            val.strip('\n')
             dict_staff[key] = val
+
     return dict_staff
 
 
