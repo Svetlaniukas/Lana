1ec642c9d2e989afc2469b6ebbaf2581bce80852
Svetlaniukas <svetlana.melichova@gmail.com>
2023-01-03 21:53:02 +0000

    ad model for db_files

--- test_my_modul_sql.py
+++ test_my_modul_sql.py
@@ -1,7 +1,4 @@
-
-
 import mymodule
-from my_sql_lite_db import time_table
 
 
 def work_with_db_sql_lite_staff_shift():
@@ -10,36 +7,37 @@ def work_with_db_sql_lite_staff_shift():
 
 def test_empty_sql_lite_shut_return_empty_dictionary():
     dict_empty = mymodule.work_with_sql_lite_db_files(
-        "staff_shift.db", 'day', 'time_table')
+        "staff_shift.db", 'day', 'time_shift')
     assert len(dict_empty) == 0
 
 
 def test_sql_lite_valid_shut_return_valid_dictionary():
     persons = mymodule.work_with_sql_lite_db_files(
-        "staff_shift.db", 'day', 'time')
-    assert persons(time_table) == len(7)
+        "staff_shift.db", 'day', 'time_shift')
+    assert len(persons) == 6
 
 
 def test_sql_lite_shut_retur_valid_dictionary():
     valid_dictionary = mymodule.work_with_sql_lite_db_files(
-        "staff_shift.db", 'time', 'date')
-    assert len(valid_dictionary) == 1
+        "staff_shift.db", 'time', 'time_shift')
+    assert valid_dictionary(
+        'Saturday', '10.am-5.pm') == 'Saturday', '10.am-5.pm'
 
 
 def test_sql_lite_shut_return_exist_day_and_time():
     file_with_data = mymodule.work_with_sql_lite_db_files(
-        "staff_shift.db", 'day', 'time')
-    assert type(file_with_data) == time_table
+        "staff_shift.db", 'day', 'time_shift')
+    assert file_with_data[0] == 'Monday', '10.am-4.pm'
 
 
 def test_7_line_sql_lite_shut_retur_7_itiems():
     line_test = mymodule.work_with_sql_lite_db_files(
         "staff_shift.db", 'day', 'time')
-    assert line_test['Monday'] == "10.am-4.pm"
+    assert len(line_test) == 3
 
 
 def test_1_line_sql_lite_shut_retur_1_itiems():
     line_test = mymodule.work_with_sql_lite_db_files(
-        "staff_shift.db", 'day', 'time'
+        "staff_shift.db", 'day', 'time_shift'
     )
-    assert line_test(time_table) == 7
+    assert len(line_test) == 
