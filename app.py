e28f04a0c66df85a84478ec4b6a3d8802eda548e
Svetlaniukas <svetlana.melichova@gmail.com>
2023-01-10 13:16:23 +0000

    try to conect db_data

--- app.py
+++ app.py
@@ -1,35 +1,54 @@
-import calendar
+import app
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template
+from flask import Flask, render_template, reguest, redirect
 
 import mymodule
 
 app = Flask(__name__)
-app.config['SQLALCHEMY DATABASE_URI'] = 'sqlite'///staff_shift.expandtabs()
+app.config['SQLALCHEMY DATABASE_URI'] = 'sqllite:///staff_shift.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 db = SQLAlchemy(app)
 
 
-class Article(db.Model:)
+class Article(db.Model):
     id = db.Colum(db.Integer, primary_key=True)
     title = db.Colum(db.Integer(100), primary_key=True)
     intro = db.Colum(db.Integer(300), primary_key=True)
     text = db.Colum(db.Integer, primary_key=True)
     date = db.Colum(db.DateTime, default=datetime.utcnow)
     
-    def_repr_(self):
-        return '<Article '
+    
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+
+@app.route('/crest-article', methhod=['POST', 'GET'])
+def create_article():
+    if reguest.method == "POST":
+        pass
+        title = reguest.form['title']
+        intro = reguest.form['intro']
+        text = reguest.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/home')
+        except:
+            return "For appload text return Error"
+    else:
+        return render_template("db_data.html")
 
 @app.route('/')
 @app.route('/home')
 def home():
     date_time = datetime.now()
-    calendar_year = calendar.TextCalendar()
     page_content = render_template("home.html",
-                                   current_time=date_time,
-                                   calendar_year=calendar_year.formatyear(
-                                       2023, w=2, l=1, c=6, m=3)
-                                   )
+            current_time=date_time,
+    )
     return page_content
 
 
@@ -43,12 +62,8 @@ def staff():
         "staff.html",
         for_week_days_shift=for_week_days_shift,
         for_staff_weekend_shift=for_staff_weekend_shift
-        )
+    )
     return page_content
-
-
-@app.route('/posts')
-def posts():
 
 
 if __name__ == "__main__":
