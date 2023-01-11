d370ea894cf7b9f611b699ec63dce7a624e94398
Svetlaniukas <svetlana.melichova@gmail.com>
2023-01-11 17:25:41 +0000

    add test for db_data

--- test_function/test_flaskr.py
+++ test_function/test_flaskr.py
@@ -1,16 +1,2 @@
-import pytest
-from app import create_article
-
-
-
-@pytest.fixture
-def client():
-    create_article.app.config['DATABASE'] = True
-    create_article.app.config['TESTING'] = True
-
-    with create_article.app.test_client() as client:
-        with create_article.app.app_context():
-            create_article.init_db()
-        yield client
 
 
