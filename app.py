e5d16b23504a68c1acaa9ec93d6b765bed250d19
Svetlaniukas <svetlana.melichova@gmail.com>
2023-01-12 17:06:14 +0000

    add

--- app.py
+++ app.py
@@ -1,44 +1,51 @@
 from datetime import datetime
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect
+from sqlalchemy import MetaData, Table, Column, Integer, String, Text, ForeignKey
 import mymodule
+
+
+
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///staff_shift.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
-db = SQLAlchemy (app)
+db = SQLAlchemy(app)
+metadata = MetaData()
+staff_name_surname = Table('staff_name_surname', metadata,
+             Column('Name', String(200), primary_key=True),
+             Column('Surname', String(200), nullable=False),
 
-
-#class Article(db.Model):
-#   id = db.Colum(db.Integer, primary_key=True)
-#   text = db.Colum(db.Integer, primary_key=True)
-#   intro = db.Colum(db.Integer(300), primary_key=True)
-#   title = db.Colum(db.Integer(100), primary_key=True)
-#    date = db.Colum(db.DateTime, default=datetime.utcnow)
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    intro = db.Column(db.String(80), unique=True, nullable=False)
+    text = db.Column(db.Text, unique=True, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
     
-    #def __repr__(self):
-        #return '<Article %r>' % self.id
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 
 @app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
-   #request.method == "POST"
-   # title = request.form['title']
-   # intro = request.form['intro']
-   # text = request.form['text']
+    if  request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
 
-        #article = Article(title=title, intro=intro, text=text)
+        article = Article(title=title, intro=intro, text=text)
 
-        #try:
-         #   db.session.add(article)
-          #  db.session.commit()
-           # return redirect('/home')
-        #except:
-            #return "For appload text return Error"
-    #else:
-    return render_template("db_data.html")
+        try:
+            db.session.add(article)
+            db.session.commit()
+            
+            return redirect('/home')
+        except:
+            return "For appload text return Error"
+    else:
+        return render_template("db_data.html")
 
 
 @app.route('/')
